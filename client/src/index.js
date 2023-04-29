3d5a6623d42967fc280d582e33fa4ae25d2a41ae
Lingzi <susanzhang0521@gmail.com>
2023-04-29 17:57:01 -0400

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactDOM } from 'react-dom/client';
+import App from './App';
+
+const app = ReactDOM.createRoot(
+  // eslint-disable-next-line no-undef
+  document.getElementById('app'),
+);
+
+app.render(<App />);
